f39a7377200ded748a9e53895091b8de08fc73eb
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-26 13:37:55 +0100

    Set up the initial state of the game

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Select elements
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+// Initital state of the game
+score0Element.innerHTML = 0; // Player 1 score
+score1Element.innerHTML = 0; // Player 2 score
+diceElement.classList.add('hidden'); // Hide the dice
