81b889b7d44ef0ee071b1566ba7217984fa470cd
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-26 14:11:27 +0100

    Switch players if 1 is rolled

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Select elements
 const score0Element = document.getElementById('score--0');
 const score1Element = document.getElementById('score--1');
+// const current0Element = document.getElementById('current--0');
+// const current1Element = document.getElementById('current--1');
 const diceElement = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn--roll');
 
@@ -21,6 +23,10 @@ const diceIcons = {
   6: "<i class='fas fa-dice-six'></i>"
 }
 
+let totalScores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 // Rolling the dice
 
 // Function to generate a random dice
@@ -29,7 +35,20 @@ const randomDice = () => {
 }
 // Add an event listener to the 'Roll Dice' button
 rollBtn.addEventListener('click', (e) => {
+  // generate a random dice number
   const num = randomDice();
   if (diceElement.classList.contains('hidden')) diceElement.classList.remove('hidden');
+  // Display the correct dice icon
   diceElement.innerHTML = diceIcons[num];
+  // Check for rolled 1
+  if (num !== 1) {
+    // Add dice to the player's current score
+    currentScore += num;
+    document.getElementById(`current--${activePlayer}`).innerHTML = currentScore;
+  } else {
+    // Switch to the other player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).innerHTML = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
 })
